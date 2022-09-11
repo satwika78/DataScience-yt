57a0e4801f3694145d358aeef5b7f75f5ded6015
satwika78 <88225043+satwika78@users.noreply.github.com>
2022-09-11 21:48:33 +0530

    Add files via upload

---
+++ 1-exercise/one.py
@@ -0,0 +1,4 @@
+#Create two variables. One to store your birth year and another one to store current year. Now calculate your age #using these two variables
+birth=2001
+current=2022
+print("Age is",current-birth)
---
+++ 1-exercise/two.py
@@ -0,0 +1,4 @@
+#Store your first, middle and last name in three different variables and then print your full name using these #variables
+first="Satwika"
+last="Gundawar"
+print(first+" "+last)
